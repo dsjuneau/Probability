06df5c246372db05a7c2d835b21b37177b4d7ced
Scott Juneau <dsjuneau@gmail.com>
2021-01-28 08:07:20 -0600

    added processing of data

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const addDay = (arr) => {
   return { d: arr[0], o: arr[1], h: arr[2], l: arr[3], c: arr[4], v: arr[5] };
 };
 
+const delta = (arr) => {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i === arr.length - 1) {
+      return newArr;
+    } else {
+      newArr.push((100 * (arr[i + 1] - arr[i])) / arr[i]);
+    }
+  }
+};
+
 stock.addEventListener("change", (e) => {
   console.log(stock.files);
   const reader = new FileReader();
@@ -20,5 +31,5 @@ stock.addEventListener("change", (e) => {
 
 getCloses.onclick = () => {
   const closes = allData.map((e) => e.c);
-  console.log(closes);
+  console.log(delta(closes).sort());
 };
