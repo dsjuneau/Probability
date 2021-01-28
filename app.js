038971a480efe9f590127f3c27e96eddd86839d4
Scott Juneau <dsjuneau@gmail.com>
2021-01-28 07:45:00 -0600

    basic data structure set up, maybe

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const stock = document.getElementById("stock");
+const getCloses = document.getElementById("get-closes");
+const allData = [];
+
+const addDay = (arr) => {
+  return { d: arr[0], o: arr[1], h: arr[2], l: arr[3], c: arr[4], v: arr[5] };
+};
 
 stock.addEventListener("change", (e) => {
   console.log(stock.files);
   const reader = new FileReader();
   reader.readAsText(stock.files[0]);
   reader.onload = () => {
-    const lines = reader.result.split("\n").map((line) => {
-      return line.split(",");
+    const lines = reader.result.split("\n").slice(1);
+    lines.forEach((line) => {
+      allData.push(addDay(line.split(",")));
     });
-    console.log(lines);
   };
 });
+
+getCloses.onclick = () => {
+  const closes = allData.map((e) => e.c);
+  console.log(closes);
+};
