47075cfb133b4e470131aecb36a367125569446a
Scott Juneau <dsjuneau@gmail.com>
2021-02-01 07:20:51 -0600

    plotter working

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 import { drawGrid } from "./modules/drawgrid.js";
-import { plot, plot2 } from "./modules/plot.js";
+import { plot } from "./modules/plot.js";
 
 const stock = document.getElementById("stock");
-const getCloses = document.getElementById("get-closes");
+
 const canvas1 = document.getElementById("canvas-1");
 const c1 = canvas1.getContext("2d");
 const allData = [];
 const cWidth = 1000;
+const pmf = {};
 
 const addDay = (arr) => {
   return { d: arr[0], o: arr[1], h: arr[2], l: arr[3], c: arr[4], v: arr[5] };
@@ -31,25 +32,37 @@ stock.addEventListener("change", (e) => {
     lines.forEach((line) => {
       allData.push(addDay(line.split(",")));
     });
+    const numSet = [];
+    numSet.length = cWidth;
+    numSet.fill(0);
+    const closes = allData.map((e) => e.c);
+    const rawPercent = delta(closes).sort(function (a, b) {
+      return a - b;
+    });
+    const interval =
+      (rawPercent[rawPercent.length - 1] - rawPercent[0]) / cWidth;
+    rawPercent.forEach((rp) => {
+      const aIndex = Math.floor((rp - rawPercent[0]) / interval);
+      if (aIndex > cWidth - 1) {
+        numSet[cWidth - 1] = numSet[cWidth - 1] + 1;
+      } else {
+        numSet[aIndex] = numSet[aIndex] + 1;
+      }
+    });
+    const sortedNumSet = [...numSet].sort((a, b) => a - b);
+    const max = sortedNumSet[cWidth - 1];
+    const min = sortedNumSet[0];
+    pmf.max = max;
+    pmf.min = min;
+    pmf.interval = interval;
+    pmf.numSet = numSet;
+    pmf.cWidth = cWidth;
+    console.log(pmf);
+    plot(pmf, c1);
   };
 });
 
-getCloses.onclick = () => {
-  const numSet = [];
-  numSet.length = cWidth;
-  numSet.fill(0);
-  const closes = allData.map((e) => e.c);
-  const rawPercent = delta(closes).sort(function (a, b) {
-    return a - b;
-  });
-  const interval = (rawPercent[rawPercent.length - 1] - rawPercent[0]) / cWidth;
-  rawPercent.forEach((rp) => {
-    const aIndex = Math.floor((rp - rawPercent[0]) / interval);
-    if (aIndex > cWidth - 1) {
-      numSet[cWidth - 1] = numSet[cWidth - 1] + 1;
-    } else {
-      numSet[aIndex] = numSet[aIndex] + 1;
-    }
-  });
-  console.log(numSet);
-};
+canvas1.width = 1200;
+canvas1.height = 800;
+
+drawGrid(c1);
--- modules/plot.js
+++ modules/plot.js
@@ -1,22 +1,13 @@
-export const plot = (numSet, c) => {
-  let interval = Math.floor(1100 / numSet.length);
-  let adjust = (1100 / numSet.length - interval) * numSet.length;
-  let left = 50 + adjust / 2;
-  let max = 0;
-  for (let i = 0; i < numSet.length; i++) {
-    if (numSet[i] > max) {
-      max = numSet[i];
-    }
-  }
-  const factor = 700 / max;
-  for (let i = 0; i < numSet.length; i++) {
+export const plot = (pmf, c) => {
+  let left = 100;
+
+  const factor = 700 / pmf.max;
+  for (let i = 0; i < pmf.numSet.length; i++) {
     c.beginPath();
     c.moveTo(left, 750);
-    c.lineTo(left, 750 - numSet[i] * factor);
+    c.lineTo(left, 750 - pmf.numSet[i] * factor);
     c.strokeStyle = "blue";
     c.stroke();
-    left = left + interval;
+    left++;
   }
 };
-
-export const plot2 = (numSet, c) => {};
