ffbffd27be87a03340383d888c69e4608ab91137
Darrell Juneau <dsjuneau@gmail.com>
2021-02-05 07:28:39 -0600

    added comments to make things clearer

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 import { plot } from "./modules/plot.js";
 
+// Connections to Dom
 const stock = document.getElementById("stock");
 const canvas1 = document.getElementById("canvas-1");
 const c1 = canvas1.getContext("2d");
 canvas1.width = 1800;
 canvas1.height = 800;
+const canvas2 = document.getElementById("canvas-2");
+const c2 = canvas2.getContext("2d");
+canvas2.width = 1800;
+canvas2.height = 800;
+
+// Global variables
 const allData = [];
 const cWidth = canvas1.width - 100;
 const pmf = {};
 
+// Function that adds a day to the allData array of price objects
 const addDay = (arr) => {
   return { d: arr[0], o: arr[1], h: arr[2], l: arr[3], c: arr[4], v: arr[5] };
 };
 
+// Function that calculates the daily price change in percentage and returns an array of these
 const delta = (arr) => {
   const newArr = [];
   for (let i = 0; i < arr.length; i++) {
@@ -24,14 +33,23 @@ const delta = (arr) => {
   }
 };
 
+// Main block of code that runs when a file is selected
 stock.addEventListener("change", (e) => {
+  //reads file
   const reader = new FileReader();
   reader.readAsText(stock.files[0]);
+
+  //once file is read this runs
   reader.onload = () => {
+    //puts file into an array of lines
     const lines = reader.result.split("\n").slice(1);
+
+    //reads each line and populates the allData array with price objects
     lines.forEach((line) => {
       allData.push(addDay(line.split(",")));
     });
+
+    //creates a pmf from the addData array
     const numSet = [];
     numSet.length = cWidth;
     numSet.fill(0);
@@ -49,18 +67,21 @@ stock.addEventListener("change", (e) => {
         numSet[aIndex] = numSet[aIndex] + 1;
       }
     });
+
+    //finds the minimum and maximum count in the pmf
     const sortedNumSet = [...numSet].sort((a, b) => a - b);
     const max = sortedNumSet[cWidth - 1];
     const min = sortedNumSet[0];
 
+    //creates an object with data for the plotter to use
     pmf.max = max;
     pmf.min = min;
     pmf.interval = interval;
     pmf.numSet = numSet;
     pmf.cWidth = cWidth;
     pmf.dataPoints = rawPercent.length;
-    console.log(pmf);
+
+    //plots data
     plot(pmf, c1, "histogram");
-    console.log(pmf);
   };
 });
